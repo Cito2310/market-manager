dc932e14a682297bdf8654f9b8886759d0787ff6
Cito2310 <cito2310@gmail.com>
2023-01-21 19:26:23 -0300

    Added testing file products

---
+++ data/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+    {
+        barcode: "7794000006195",
+        brand: "Hellmann's",
+        size: "60g",
+        type: "Ketchup",
+    },
+    {
+        barcode: "7794000006195",
+        brand: "Hellmann's",
+        size: "125cm3 | 118g",
+        type: "Mayonesa",
+    },
+    {
+        barcode: "7794000006195",
+        brand: "Savora",
+        size: "60g",
+        type: "Mostaza",
+    },
+]
