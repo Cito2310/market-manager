d37cfc47ded88d3ab1b8755963239589bd06fbbc
Cito2310 <cito2310@gmail.com>
2023-02-09 23:24:41 -0300

    Added electron to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useContext } from "react";
-
-import { ContextBarcode } from './Provider/ProviderInputBarcode';
-import { ScreenCashRegister } from "./screen_cash_register/ScreenCashRegister";
-
 function App() {
-  const { barcode, setBarcode } = useContext(ContextBarcode)
-
   return (
     <div className="App">
-      <ScreenCashRegister></ScreenCashRegister>
+      hello world
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
