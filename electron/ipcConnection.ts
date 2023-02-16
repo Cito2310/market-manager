8ae3c64b197e18a383c78a58175a3740f4a24887
Cito2310 <cito2310@gmail.com>
2023-02-16 09:19:43 -0300

    added ipcConnection save database products

--- electron/ipcConnection.ts
+++ electron/ipcConnection.ts
@@ -1,10 +1,14 @@
 import { ipcMain, ipcRenderer } from 'electron';
-import { writeFileSync } from 'fs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 
 import { ipcNames } from '../Types/ipcNames';
+import * as path from 'path';
+import axios from 'axios';
 
 export const ipcConnections = () => {
-    ipcMain.on("get-data-products" as ipcNames, (e, args) => {
-        writeFileSync("./text.txt", args)
+    ipcMain.on("get-data-products" as ipcNames, async (e, args) => {
+        if (!existsSync("./database")) mkdirSync("./database");
+        const resp = await axios.get("https://market-product-rest.onrender.com/api/product/", {headers: {token: args}})
+        writeFileSync("./database/data_products.json", JSON.stringify(resp.data))
     })
 }
