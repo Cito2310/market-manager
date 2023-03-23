4067bb99acec4f97d2478e97bef163c51d28c362
Cito2310 <cito2310@gmail.com>
2023-03-23 10:57:21 -0300

    added style btnIcon

--- src/DEV_Component_Styles/TestStyle.tsx
+++ src/DEV_Component_Styles/TestStyle.tsx
@@ -1,13 +1,16 @@
 import "./test-style.scss";
 import "./btn.scss";
 import { SvgElements } from '../components/SvgElements';
+import { BtnIcon } from './BtnIcon';
 
 export const TestStyle = () => {
+    // const button = () => {console.log(event.target)} 
+
     return (
         <div className="screen-test column">
             <h1>TEST STYLE</h1>
 
-            <div className="row gap-16">
+            {/* <div className="row gap-16">
                 <button className="btn primary">PRIMARY</button>
                 <button className="btn white">WHITE</button>
                 <button className="btn alert">ALERT</button>
@@ -16,7 +19,20 @@ export const TestStyle = () => {
                 <button disabled className="btn loading w6"><SvgElements element="spinner"/></button>
                 <button disabled className="btn done w6"><SvgElements element="done"/> Done</button>
                 <button disabled className="btn error w6"><SvgElements element="xmark"/> Error</button>
-            </div>
+            </div> */}
+
+            {/* <div className="row gap-16">
+                <button onClick={button}></button>
+                <BtnIcon onClickFunc={button} element="done"/>
+                <BtnIcon element="done"/>
+                <BtnIcon element="lens"/>
+                <BtnIcon element="pencil"/>
+                <BtnIcon element="plus" color="green"/>
+                <BtnIcon element="print"/>
+                <BtnIcon element="spinner"/>
+                <BtnIcon element="trash" color="red"/>
+                <BtnIcon element="xmark"/>
+            </div> */}
 
         </div>
     )
