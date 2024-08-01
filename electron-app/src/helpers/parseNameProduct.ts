ab6a25254033d18259c8c927c0744d2346444c86
Cito2310 <cito2310@gmail.com>
2024-08-01 10:50:42 -0300

    fixed error show-subcategory

--- electron-app/src/helpers/parseNameProduct.ts
+++ electron-app/src/helpers/parseNameProduct.ts
@@ -3,7 +3,7 @@ import { Product } from "../../Types";
 export const parseNameProduct = (product: Product): string => {
     const { brand, name, size, typeSize, subCategory, type } = product;
 
-    const nameBase = `${subCategory} ${brand} ${name}`;
+    const nameBase = `${brand} ${name}`;
     const sizeParse = ( type === "weight" ) ?  ""  :  size+typeSize;
 
     return `${nameBase} ${sizeParse}`;
